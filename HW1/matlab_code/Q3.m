4292a0449da0e4c2725a7f2e8d91304660d69bf4
AlmogDob <dobalmog123@gmail.com>
2025-05-26 00:22:03 +0300

    [HW1][matlab] finished code hopfully

--- HW1/matlab_code/Q3.m
+++ HW1/matlab_code/Q3.m
@@ -11,7 +11,7 @@ fig1 = figure('Name', '1', 'Position', [100, 250, 900, 600]);
 hold all
 
 steps = [];
-[end_x_vec, end_y_vec] = one_run(0, 0, step_size, num_of_steps, num_of_walks);
+[end_x_vec, end_y_vec, ~] = one_run(0, 0, step_size, num_of_steps, num_of_walks);
 
 [count_of_band, count_of_outer_circ] = num_points_in_band_and_outer_circ(r, dr, end_x_vec, end_y_vec);
 
@@ -32,26 +32,28 @@ axis equal
 box on
 grid on
 grid minor
-
+% exportgraphics(fig1, 'images/graph1.png','Resolution',300);
 
 %% Q.3.a PREFORM AND PRINT A RUN AND PDF ###############################
 step_size = 1; % [m]
 num_of_steps     = 1e2;
 dr               = 0.5;
-% num_of_walks_vec = [4e5, 3e5, 2e5, 1e5, 7.5e4, 5e4, 2.5e4, 1e4, 7.5e3, 5e3, 2.5e3, 1e3];
-num_of_walks_vec = [7.55e4, 5e4, 2.5e4, 1e4, 5e3, 1e3];
+num_of_walks_vec = [4e5, 3e5, 2e5, 1e5, 7.5e4, 5e4, 2.5e4, 1e4, 7.5e3, 5e3, 2.5e3, 1e3];
+% num_of_walks_vec = [7.5e4, 5e4, 2.5e4, 1e4, 5e3, 1e3];
 num_of_radiuses  = 1e2;
 
-end_x_vec_vec = {};
-end_y_vec_vec = {};
-rs_vec        = {};
-pdf_vec_vec   = {};
+end_x_vec_vec        = {};
+end_y_vec_vec        = {};
+distances_vec_of_vec = {};
+rs_vec               = {};
+pdf_vec_vec          = {};
 for index = 1:length(num_of_walks_vec)
     num_of_walks = num_of_walks_vec(index);
     steps = [];
-    [current_end_x_vec, current_end_y_vec] = one_run(0, 0, step_size, num_of_steps, num_of_walks);
+    [current_end_x_vec, current_end_y_vec, current_distances_vec] = one_run(0, 0, step_size, num_of_steps, num_of_walks);
     end_x_vec_vec{index,1} = current_end_x_vec;
     end_y_vec_vec{index,1} = current_end_y_vec;
+    distances_vec_of_vec{index,1} = current_distances_vec;
     rs_vec{index,1} = linspace(0, min(max(abs(current_end_x_vec)),max(abs(current_end_y_vec))), num_of_radiuses);
     pdf_vec_vec{index,1} = calc_pdf(rs_vec{index,1}, dr, current_end_x_vec, current_end_y_vec);
 end
@@ -122,25 +124,29 @@ for index = 1:length(num_of_walks_vec)
     grid on
     grid minor
 end
-    
+% exportgraphics(fig2, 'images/graph2.png','Resolution',300);
+% exportgraphics(fig1, 'images/graph1.png','Resolution',300); exportgraphics(fig2, 'images/graph2.png','Resolution',300);
+
 %% Q.3.b Average Displacement ##########################################
 % step_size = 1; % [m]
 % num_of_steps     = 1e2;
 % dr               = 0.5;
-% num_of_walks_vec = [3e5, 2e5, 1e5, 7.5e4, 5e4, 2.5e4, 1e4, 5e3, 1e3];
-% % num_of_walks_vec = [7.55e4, 5e4, 2.5e4, 1e4, 5e3, 1e3];
+% % num_of_walks_vec = [4e5, 3e5, 2e5, 1e5, 7.5e4, 5e4, 2.5e4, 1e4, 7.5e3, 5e3, 2.5e3, 1e3];
+% num_of_walks_vec = [7.55e4, 5e4, 2.5e4, 1e4, 5e3, 1e3];
 % num_of_radiuses  = 1e2;
 % 
-% end_x_vec_vec = {};
-% end_y_vec_vec = {};
-% rs_vec        = {};
-% pdf_vec_vec   = {};
+% end_x_vec_vec        = {};
+% end_y_vec_vec        = {};
+% distances_vec_of_vec = {};
+% rs_vec               = {};
+% pdf_vec_vec          = {};
 % for index = 1:length(num_of_walks_vec)
 %     num_of_walks = num_of_walks_vec(index);
 %     steps = [];
-%     [current_end_x_vec, current_end_y_vec] = one_run(0, 0, step_size, num_of_steps, num_of_walks);
+%     [current_end_x_vec, current_end_y_vec, current_distances_vec] = one_run(0, 0, step_size, num_of_steps, num_of_walks);
 %     end_x_vec_vec{index,1} = current_end_x_vec;
 %     end_y_vec_vec{index,1} = current_end_y_vec;
+%     distances_vec_of_vec{index,1} = current_distances_vec;
 %     rs_vec{index,1} = linspace(0, min(max(abs(current_end_x_vec)),max(abs(current_end_y_vec))), num_of_radiuses);
 %     pdf_vec_vec{index,1} = calc_pdf(rs_vec{index,1}, dr, current_end_x_vec, current_end_y_vec);
 % end
@@ -160,12 +166,16 @@ title('Average Displacment as a Function of # of realization')
 box on
 grid on
 grid minor
+% exportgraphics(fig3, 'images/graph3.png','Resolution',300);
+% exportgraphics(fig3, 'images/graph3.png','Resolution',300); exportgraphics(fig1, 'images/graph1.png','Resolution',300); exportgraphics(fig2, 'images/graph2.png','Resolution',300);
 
 %% Q.3.c autocovariance and autocorrelation ############################
 step_size = 1; % [m]
 number_of_steps = 1e2;
 
-fig4 = figure('Name', '4', 'Position', [450, 250, 900, 600]);
+fig4 = figure('Name', '4', 'Position', [450, 250, 1350, 400]);
+
+subplot(1,3,1) % #######################################################
 
 hold all
 steps = [];
@@ -204,9 +214,9 @@ for i = 1:length(distances)
     Q(i) = C(1,2);
 end
 
-fig5 = figure('Name', '5', 'Position', [600, 250, 900, 600]);
+% fig5 = figure('Name', '5', 'Position', [600, 250, 900, 600]);
 
-subplot(1,2,1) % #######################################################
+subplot(1,3,2) % #######################################################
 plot(1:length(distances), Q)
 
 xlabel('\# of steps [m]','FontSize',14,'Interpreter','latex')
@@ -216,7 +226,7 @@ box on
 grid on
 grid minor
 
-subplot(1,2,2) % #######################################################
+subplot(1,3,3) % #######################################################
 plot(1:length(distances), Q/Q(1))
 
 xlabel('\# of steps [m]','FontSize',14,'Interpreter','latex')
@@ -225,12 +235,119 @@ title('Autocovariance as a Function of Steps')
 box on
 grid on
 grid minor
+% exportgraphics(fig4, 'images/graph4.png','Resolution',300);
+% exportgraphics(fig4, 'images/graph4.png','Resolution',300); exportgraphics(fig3, 'images/graph3.png','Resolution',300); exportgraphics(fig1, 'images/graph1.png','Resolution',300); exportgraphics(fig2, 'images/graph2.png','Resolution',300);
 
 %% Q.3.c ensemble ######################################################
+% step_size = 1; % [m]
+% num_of_steps     = 1e2;
+% dr               = 0.5;
+% % num_of_walks_vec = [4e5, 3e5, 2e5, 1e5, 7.5e4, 5e4, 2.5e4, 1e4, 7.5e3, 5e3, 2.5e3, 1e3];
+% num_of_walks_vec = [7.55e4, 5e4, 2.5e4, 1e4, 5e3, 1e3];
+% num_of_radiuses  = 1e2;
+% 
+% end_x_vec_vec        = {};
+% end_y_vec_vec        = {};
+% distances_vec_of_vec = {};
+% rs_vec               = {};
+% pdf_vec_vec          = {};
+% for index = 1:length(num_of_walks_vec)
+%     num_of_walks = num_of_walks_vec(index);
+%     steps = [];
+%     [current_end_x_vec, current_end_y_vec, current_distances_vec] = one_run(0, 0, step_size, num_of_steps, num_of_walks);
+%     end_x_vec_vec{index,1} = current_end_x_vec;
+%     end_y_vec_vec{index,1} = current_end_y_vec;
+%     distances_vec_of_vec{index,1} = current_distances_vec;
+%     rs_vec{index,1} = linspace(0, min(max(abs(current_end_x_vec)),max(abs(current_end_y_vec))), num_of_radiuses);
+%     pdf_vec_vec{index,1} = calc_pdf(rs_vec{index,1}, dr, current_end_x_vec, current_end_y_vec);
+% end
 
+Q_vec = {};
+Q_ensemble = {};
+for index = 1:length(num_of_walks_vec)
+    fprintf('Walk number %d\n', index);
+    distances_vec = distances_vec_of_vec{index,1};
+    Q_ensemble{index,1} = zeros(1,length(distances));
+    for j = 1:length(distances_vec)
+        j
+        distances = distances_vec{j,1};
+        Q = [];
+        for i = 1:length(distances)
+            A = distances(1:length(distances)-i);
+            B = distances(1+i:length(distances));
+            C = cov(A, B);
+            Q(i) = C(1,2);
+        end
+        Q_ensemble{index,1} = Q_ensemble{index,1} + Q;
+    end
+    Q_ensemble{index,1} = Q_ensemble{index,1} / num_of_walks_vec(index);
+end
+%%
+fig5 = figure('Name', '5', 'Position', [600, 250, 900, 600]);
 
+colors = jet(length(num_of_walks_vec));
+% colors = cool(length(num_of_walks_vec))*0.8;
+subplot(1,2,1) % #######################################################
+hold all
+lg  = {};
+for index = 1:length(num_of_walks_vec)
+    Q = Q_ensemble{index,1};
+    plot(1:length(distances), Q, '-', 'LineWidth', 2, 'Color', colors(index,:))
+    lg{end+1} = sprintf('# of realization: %d', num_of_walks_vec(index));
+end
 
+xlabel('\# of steps [m]','FontSize',14,'Interpreter','latex')
+ylabel('autocovariance [m]','FontSize',14,'Interpreter','latex')
+title('Autocovariance as a Function of Steps - Full Ensemble')
+legend(lg)
+box on
+grid on
+grid minor
 
+zoom = axes('position',[0.25 0.36 0.2 0.2]);
+box on % put box around new pair of axes
+hold all
+for index = 1:length(num_of_walks_vec)
+    Q = Q_ensemble{index,1};
+    plot(1:length(distances), Q, '-', 'LineWidth', 2, 'Color', colors(index,:))
+end
+zoom.XLim = [27.8, 28.6];
+zoom.YLim = [2.29, 2.36];
+grid on
+grid minor
+
+subplot(1,2,2) % #######################################################
+hold all
+lg  = {};
+for index = 1:length(num_of_walks_vec)
+    Q = Q_ensemble{index,1};
+    plot(1:length(distances), Q/Q(1), '-', 'LineWidth', 2, 'Color', colors(index,:))
+    hold on
+    lg{end+1} = sprintf('# of realization: %d', num_of_walks_vec(index));
+end
+
+xlabel('\# of steps [m]','FontSize',14,'Interpreter','latex')
+ylabel('autocovariance [m]','FontSize',14,'Interpreter','latex')
+title('Autocovariance as a Function of Steps - Full Ensemble')
+legend(lg)
+box on
+grid on
+grid minor
+
+zoom = axes('position',[0.69 0.36 0.2 0.2]);
+box on % put box around new pair of axes
+hold all
+for index = 1:length(num_of_walks_vec)
+    Q = Q_ensemble{index,1};
+    plot(1:length(distances), Q/Q(1), '-', 'LineWidth', 2, 'Color', colors(index,:))
+end
+zoom.XLim = [27.8, 28.6];
+zoom.YLim = [0.255, 0.258];
+grid on
+grid minor
+
+% exportgraphics(fig5, 'images/graph5.png','Resolution',300);
+% exportgraphics(fig5, 'images/graph5.png','Resolution',300); exportgraphics(fig4, 'images/graph4.png','Resolution',300); exportgraphics(fig3, 'images/graph3.png','Resolution',300); exportgraphics(fig1, 'images/graph1.png','Resolution',300); exportgraphics(fig2, 'images/graph2.png','Resolution',300);
 
 
 
@@ -245,25 +362,29 @@ function [des_x, des_y] = one_step(src_x, src_y, step_size)
     des_y = src_y + step_size * sin(theta);    
 end
 
-function [end_x, end_y] = one_walk(start_x, start_y, step_size, num_of_steps)
+function [end_x, end_y, distances] = one_walk(start_x, start_y, step_size, num_of_steps)
     x = start_x;
     y = start_y;
+    distances = [];
     for i=1:num_of_steps
         [x,y] = one_step(x, y, step_size);
+        distances(i) = sqrt(x^2+y^2);
     end
     end_x = x;
     end_y = y;
 end
 
-function [end_x_vec, end_y_vec] = one_run(start_x, start_y, step_size, num_of_steps, num_of_walks)
+function [end_x_vec, end_y_vec, distances_vec] = one_run(start_x, start_y, step_size, num_of_steps, num_of_walks)
     fprintf('preforming a run ...\n');
     steps = [];
+    distances_vec = {};
     for i=1:num_of_walks
         if ~mod(i, num_of_walks/10)
             fprintf('   complited: %2.0f%%\n', i/num_of_walks*100);
         end
-        [x,y] = one_walk(start_x, start_y, step_size, num_of_steps);
+        [x,y,distances] = one_walk(start_x, start_y, step_size, num_of_steps);
         steps(end+1,:) = [x,y];
+        distances_vec{i, 1} = distances;
     end
     end_x_vec = steps(:,1);
     end_y_vec = steps(:,2);
