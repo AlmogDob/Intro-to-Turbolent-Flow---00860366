542f6855e527abd60cdf1e25fdef1de24f5f53b0
AlmogDob <dobalmog123@gmail.com>
2025-05-25 17:28:12 +0300

    [HW1][matlab] done Q3.b

--- HW1/matlab_code/Q3.m
+++ HW1/matlab_code/Q3.m
@@ -38,8 +38,8 @@ grid minor
 step_size = 1; % [m]
 num_of_steps     = 1e2;
 dr               = 0.5;
-% num_of_walks_vec = [3e5, 2e5, 1e5, 7.5e4, 5e4, 2.5e4, 1e4, 5e3, 1e3];
-num_of_walks_vec = [7.55e4, 5e4, 2.5e4, 1e4, 5e3, 1e3];
+num_of_walks_vec = [4e5, 3e5, 2e5, 1e5, 7.5e4, 5e4, 2.5e4, 1e4, 7.5e3, 5e3, 2.5e3, 1e3];
+% num_of_walks_vec = [7.55e4, 5e4, 2.5e4, 1e4, 5e3, 1e3];
 num_of_radiuses  = 1e2;
 
 end_x_vec_vec = {};
@@ -113,7 +113,7 @@ for index = 1:length(num_of_walks_vec)
     
     rms(end+1) = sqrt(sum((analytical_solution-pdf_vec).^2));
     
-    plot(flip(num_of_walks_vec(end-length(rms)+1:end)), rms, '-', 'LineWidth', 2, 'Color', 'k')
+    semilogx(flip(num_of_walks_vec(end-length(rms)+1:end)), rms, '-', 'LineWidth', 2, 'Color', 'k')
     
     xlabel('\# realization [-]','FontSize',14,'Interpreter','latex')
     ylabel('RMS [-]','FontSize',14,'Interpreter','latex')
@@ -124,10 +124,42 @@ for index = 1:length(num_of_walks_vec)
 end
     
 %% Q.3.b Average Displacement ########################################
+% step_size = 1; % [m]
+% num_of_steps     = 1e2;
+% dr               = 0.5;
+% num_of_walks_vec = [3e5, 2e5, 1e5, 7.5e4, 5e4, 2.5e4, 1e4, 5e3, 1e3];
+% % num_of_walks_vec = [7.55e4, 5e4, 2.5e4, 1e4, 5e3, 1e3];
+% num_of_radiuses  = 1e2;
+% 
+% end_x_vec_vec = {};
+% end_y_vec_vec = {};
+% rs_vec        = {};
+% pdf_vec_vec   = {};
+% for index = 1:length(num_of_walks_vec)
+%     num_of_walks = num_of_walks_vec(index);
+%     steps = [];
+%     [current_end_x_vec, current_end_y_vec] = one_run(0, 0, step_size, num_of_steps, num_of_walks);
+%     end_x_vec_vec{index,1} = current_end_x_vec;
+%     end_y_vec_vec{index,1} = current_end_y_vec;
+%     rs_vec{index,1} = linspace(0, min(max(abs(current_end_x_vec)),max(abs(current_end_y_vec))), num_of_radiuses);
+%     pdf_vec_vec{index,1} = calc_pdf(rs_vec{index,1}, dr, current_end_x_vec, current_end_y_vec);
+% end
 
+ave_disp = [];
+for index = 1:length(num_of_walks_vec)
+    ave_disp(index) = trapz(rs_vec{index,:}, rs_vec{index,:}.*pdf_vec_vec{index,:});
+end
 
+fig3 = figure('Name', '3', 'Position', [300, 250, 900, 600]);
 
-
+semilogx(num_of_walks_vec, ave_disp, '-', 'LineWidth', 2, 'Color', 'k')
+    
+xlabel('\# realization [-]','FontSize',14,'Interpreter','latex')
+ylabel('average displacment [m]','FontSize',14,'Interpreter','latex')
+title('Average Displacment as a Function of # of realization')
+box on
+grid on
+grid minor
 
 
 
